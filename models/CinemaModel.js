9af5c539277ccb8f16e468bf09179a4aa092da06
NeelDQA <NDudhia@qa.com>
2021-06-04 11:44:54 +0100

    QAC-11 #comment ND: Adding cinema details completed back-end

--- models/CinemaModel.js
+++ models/CinemaModel.js
@@ -1,50 +1,47 @@
 module.exports = mongoose => {
     const CinemaModel = mongoose.model(
         "cinema",
-        mongoose.Schema(
-            {
-            name:String,
-            location: [{
-                address: String,
-                coordinates: [{
-                    lat: Number,
-                    lng: Number
-                }]
-            }],
+        mongoose.Schema({
+                name: String,
+                location: {
+                    address: String,
+                    lat: String,
+                    lng: String
+                },
 
-            opening_times: [{
-                Mon: [{
-                    open: String,
-                    close: String
-                }],
-                Tue: [{
-                    open: String,
-                    close: String
-                }],
-                Wed: [{
-                    open: String,
-                    close: String
-                }],
-                Thurs: [{
-                    open: String,
-                    close: String
-                }],
-                Fri: [{
-                    open: String,
-                    close: String
-                }],
-                Sat: [{
-                    open: String,
-                    close: String
-                }],
-                Sun: [{
-                    open: String,
-                    close: String
-                }]
-            }],
-            type: String
-        },
-        {timestamps:true}
+                opening_times: {
+                    Mon: {
+                        open: String,
+                        close: String
+                    },
+                    Tue: {
+                        open: String,
+                        close: String
+                    },
+                    Wed: {
+                        open: String,
+                        close: String
+                    },
+                    Thurs: {
+                        open: String,
+                        close: String
+                    },
+                    Fri: {
+                        open: String,
+                        close: String
+                    },
+                    Sat: {
+                        open: String,
+                        close: String
+                    },
+                    Sun: {
+                        open: String,
+                        close: String
+                    }
+                },
+                type: String
+            },
+
         )
     );
     return CinemaModel;
